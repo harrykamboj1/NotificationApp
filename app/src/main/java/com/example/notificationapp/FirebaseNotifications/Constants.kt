c9408624956c4cbd3942f86138fabc4a105a6d2d
Harnoor Singh <49549662+harrykamboj1@users.noreply.github.com>
2021-03-16 11:07:15 +0530

    Update Constants.kt

--- app/src/main/java/com/example/notificationapp/FirebaseNotifications/Constants.kt
+++ app/src/main/java/com/example/notificationapp/FirebaseNotifications/Constants.kt
@@ -4,7 +4,7 @@ class Constants {
 
     companion object{
         const val BASE_URL  = "https://fcm.googleapis.com"
-        const val SERVER_KEY = "AAAAMheOaDU:APA91bHBjCPw2mdtmdZYgWoINqd_SykRE3-O8lWJLM8WtKGiV8lLsCGkByxUemFbnJAT6Za74ITjoNtbIVxBs9mr7NoHKCghNTWRVUOI4QFt65-FK_r2mA5OJd2xz_FrEKTOhX34KPtu"
+        const val SERVER_KEY = ""
         const val  CONTENT_TYPE = "application/json"
     }
 }
